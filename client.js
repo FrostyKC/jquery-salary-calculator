7604fb73712e080f99df1f649fdadcc5e4ef5437
Joe Thompson <smnindian30@yahoo.com>
2020-10-09 16:35:00 -0500

    capture input info

--- client.js
+++
@@ -1,6 +0,0 @@
-console.log('hi');
-$(document).ready(goodToGo);
-
-function goodToGo() {
-  console.log('ready');
-}
---
+++ scripts/client.js
@@ -0,0 +1,18 @@
+$(document).ready(goodToGo);
+
+//A 'Submit' button should collect the form information, store the information to calculate monthly costs,
+// append information to the DOM and clear the input fields.
+
+function goodToGo() {
+  console.log('ready');
+  $('.js-employeeSubmit').on('click', clickSubmit);
+}
+function clickSubmit() {
+  // capture input info
+  const firstName = $('.js-firstName').val();
+  const lastName = $('.js-lastName').val();
+  const idNumber = $('.js-idNumber').val();
+  const jobTitle = $('.js-jobTitle').val();
+  const annualSalary = $('.js-annualSalary').val();
+  console.log(firstName, lastName, idNumber, jobTitle, annualSalary);
+}
